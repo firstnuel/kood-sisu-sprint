a3e747cccb6de8ea66fb3172dfda1c43f951964c
firstnuel <99360864+firstnuel@users.noreply.github.com>
2024-10-29 11:41:23 +0200

    added str_concat.go file

--- str_concat.go
+++ str_concat.go
@@ -1,8 +1,5 @@
 package sprint
 
-import "fmt"
-
 func StrConcat(s1, s2, delim string) string {
-	concatedStr := fmt.Sprintf("%v%v%v", s1, delim, s2)
-	return concatedStr
+	return s1 + delim + s2
 }
