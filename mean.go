ea9ada86de3f33911b6ab82372db1415b8120f03
firstnuel <99360864+firstnuel@users.noreply.github.com>
2024-10-29 11:27:10 +0200

    added mean.go file

--- mean.go
+++ mean.go
@@ -1,9 +1,6 @@
 package sprint
 
-import "math"
-
 func Mean(a, b, c float32) float32 {
 	mean := (a + b + c) / 3
-	rounded := float32(math.Round(float64(mean)*10) / 10)
-	return rounded
+	return mean
 }
