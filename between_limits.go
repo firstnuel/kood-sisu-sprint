4e7ca61bf75a4c668213b6ae6f1d6a164532fe62
firstnuel <99360864+firstnuel@users.noreply.github.com>
2024-10-30 10:19:23 +0200

    Added func BetweenLimits

--- between_limits.go
+++ between_limits.go
@@ -1,12 +1,9 @@
 package sprint
 
-import "fmt"
-
 func BetweenLimits(from, to rune) string {
 	acc := ""
 	for i := to + 1; i < from; i++ {
-		str := fmt.Sprintf("%c", i)
-		acc += str
+		acc += string(i)
 	}
 	return acc
 }
