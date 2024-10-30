803d5659864b6fb053f421e3ca496c9ab79eeec8
firstnuel <99360864+firstnuel@users.noreply.github.com>
2024-10-30 12:40:03 +0200

    added leap year

--- is_leap_year.go
+++ is_leap_year.go
@@ -1,8 +1,10 @@
 package sprint
 
 func IsLeapYear(year int) bool {
-	if year%100 == 0 && year%400 == 0 {
-		return true
+	if year%4 == 0 {
+		if year%100 == 0 && year%400 == 0 {
+			return true
+		}
 	}
 	return false
 }
