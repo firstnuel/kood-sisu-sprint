3e5f350bebb2cad42f4a9708b6d4fb81fa5e5349
firstnuel <99360864+firstnuel@users.noreply.github.com>
2024-10-31 10:13:46 +0200

    updated combinations solution

--- combinations.go
+++ combinations.go
@@ -5,8 +5,8 @@ import "fmt"
 func Combinations() string {
 	acc := ""
 	for i := 0; i <= 7; i++ {
-		for j := 0; j <= 8; j++ {
-			for k := 0; k <= 9; k++ {
+		for j := i + 1; j <= 8; j++ {
+			for k := j + 1; k <= 9; k++ {
 				acc += fmt.Sprintf("%v%v%v", i, j, k)
 				if !(i == 7 && j == 8 && k == 9) {
 					acc += ", "
