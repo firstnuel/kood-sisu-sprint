7a033e3cf8810607a1754867a2314a6b459708b4
firstnuel <99360864+firstnuel@users.noreply.github.com>
2024-10-31 10:03:28 +0200

    updated pairs solution

--- pairs.go
+++ pairs.go
@@ -5,7 +5,7 @@ import "fmt"
 func Pairs() string {
 	acc := ""
 	for i := 0; i < 99; i++ {
-		for j := 0; j < 100; j++ {
+		for j := 1; j < 100; j++ {
 			acc += fmt.Sprintf("%02d %02d", i, j)
 			if !(i == 98 && j == 99) {
 				acc += ", "
