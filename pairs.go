d1b0f25c677a529e7f1feea5aae9606ce144d0cc
firstnuel <99360864+firstnuel@users.noreply.github.com>
2024-10-31 09:56:42 +0200

    added pairs solution

---
+++ pairs.go
@@ -0,0 +1,16 @@
+package sprint
+
+import "fmt"
+
+func Pairs() string {
+	acc := ""
+	for i := 0; i < 99; i++ {
+		for j := 0; j < 100; j++ {
+			acc += fmt.Sprintf("%02d %02d", i, j)
+			if !(i == 98 && j == 99) {
+				acc += ", "
+			}
+		}
+	}
+	return acc
+}
