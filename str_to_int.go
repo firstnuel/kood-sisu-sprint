7155a7c78c8da4e70ee2568e1dc579880f041ab7
firstnuel <99360864+firstnuel@users.noreply.github.com>
2024-10-31 10:41:31 +0200

    added soln for StrToInt

---
+++ str_to_int.go
@@ -0,0 +1,30 @@
+package sprint
+
+func StrToInt(s string) int {
+	start, acc := 0, 0
+	IsNegative := false
+
+	if s[0] == '-' {
+		start = 1
+		IsNegative = true
+	}
+
+	if s[0] == '+' {
+		start = 1
+	}
+
+	for i := start; i < len(s); i++ {
+		if s[i] < '0' || s[i] > '9' {
+			return 0
+		}
+		if acc != 0 || s[i] != '0' {
+			digit := int(s[i] - '0')
+			acc = acc*10 + digit
+		}
+	}
+
+	if IsNegative {
+		return -acc
+	}
+	return acc
+}
