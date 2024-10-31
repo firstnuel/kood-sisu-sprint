da9fa78c85ab2fead9dc58338878d3e01c026633
firstnuel <99360864+firstnuel@users.noreply.github.com>
2024-10-31 10:27:24 +0200

    updated countdown soln

--- countdown.go
+++ countdown.go
@@ -1,9 +1,18 @@
 package sprint
 
 func Countdown(n int) string {
+	if n <= 0 {
+		return "0!"
+	}
 	acc := ""
 	for i := n; i >= 0; i -= 2 {
-		acc += string(i+'0') + ", "
+		acc += string(i + '0')
+		if i != 0 {
+			acc += ", "
+		}
 	}
-	return acc + "0!"
+	if n%2 != 0 {
+		return acc + "0!"
+	}
+	return acc + "!"
 }
