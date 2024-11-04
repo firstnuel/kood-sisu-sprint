d86646443f42c037b992e37df6b9da0d0e048074
firstnuel <99360864+firstnuel@users.noreply.github.com>
2024-11-04 16:22:21 +0200

    added bulkAtoI func

--- bulk_atoi.go
+++ bulk_atoi.go
@@ -1,10 +1,39 @@
 package sprint
 
+func strToInt(s string) int {
+	start, acc := 0, 0
+	IsNegative := false
+
+	if s[0] == '-' {
+		start = 1
+		IsNegative = true
+	}
+
+	if s[0] == '+' {
+		start = 1
+	}
+
+	for i := start; i < len(s); i++ {
+		if s[i] < '0' || s[i] > '9' {
+			return 0
+		}
+		if acc != 0 || s[i] != '0' {
+			digit := int(s[i] - '0')
+			acc = acc*10 + digit
+		}
+	}
+
+	if IsNegative {
+		return -acc
+	}
+	return acc
+}
+
 func BulkAtoi(arr []string) []int {
 	newArr := make([]int, len(arr))
 
 	for i, v := range arr {
-		newArr[i] = StrToInt(v)
+		newArr[i] = strToInt(v)
 	}
 
 	return newArr
