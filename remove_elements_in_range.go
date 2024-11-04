4ca8e161103a1702b78b856ff15a1707ab71b708
firstnuel <99360864+firstnuel@users.noreply.github.com>
2024-11-04 11:37:29 +0200

    added remove elements in range soln

--- remove_elements_in_range.go
+++ remove_elements_in_range.go
@@ -5,14 +5,14 @@ func RemoveElementsInRange(arr []float64, from, to int) []float64 {
 	if from > to {
 		from, to = to, from
 	}
-	if from < 0 || from > len(arr) {
-		return nil
-	}
 	if to > len(arr)-1 {
 		if from < 0 || from > len(arr) {
 			return nil
 		}
 		return arr[:from]
+	}
+	if from < 0 || from > len(arr) {
+		return nil
 	}
 	newArr := append(arr[:from], arr[to:]...)
 	return newArr
