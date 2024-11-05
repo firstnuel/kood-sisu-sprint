fafe5c3a6190d330407deedbc9857ab9b7977f4f
firstnuel <99360864+firstnuel@users.noreply.github.com>
2024-11-05 15:39:33 +0200

    Addded to capital case

--- to_capital_case.go
+++ to_capital_case.go
@@ -1,32 +1,32 @@
 package sprint
 
-func titleCase(s string) string {
-	if len(s) == 0 {
-		return s
-	}
-	if rune(s[0]) >= 97 && rune(s[0]) <= 122 {
-		return string(rune(s[0])-32) + s[1:]
-	}
-	return s
-}
-
 func ToCapitalCase(s string) string {
-	newStr := ""
-	n := 0
+	var result string
+	capitalizeNext := true
 
 	for i := 0; i < len(s); i++ {
-		if !((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z')) {
-			if n < i {
-				word := s[n:i]
-				newStr += titleCase(word)
+		ch := s[i]
+
+		if ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z' {
+			if capitalizeNext {
+				// Convert lowercase letter to uppercase if needed
+				if ch >= 'a' && ch <= 'z' {
+					ch -= 'a' - 'A'
+				}
+				capitalizeNext = false
+			} else {
+				// Convert uppercase letter to lowercase if needed
+				if ch >= 'A' && ch <= 'Z' {
+					ch += 'a' - 'A'
+				}
 			}
-			newStr += string(s[i])
-			n = i + 1
+		} else {
+			// If not a letter, reset to capitalize the next letter
+			capitalizeNext = true
 		}
+
+		result += string(ch)
 	}
-	if n < len(s) {
-		word := s[n:]
-		newStr += titleCase(word)
-	}
-	return newStr
+
+	return result
 }
