200b14434a19b2396e2da61721ad904a84c137ba
firstnuel <99360864+firstnuel@users.noreply.github.com>
2024-11-05 18:54:44 +0200

    added split whitespaces

---
+++ split_whitespaces.go
@@ -0,0 +1,17 @@
+package sprint
+
+func SplitWhitespaces(s string) []string {
+	newStr := []string{}
+	start, end := 0, 0
+
+	for i, v := range s {
+		if v == ' ' {
+			end = i
+			if start < end {
+				newStr = append(newStr, s[start:end])
+			}
+			start = i + 1
+		}
+	}
+	return newStr
+}
