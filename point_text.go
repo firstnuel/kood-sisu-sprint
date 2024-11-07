0686d0219b0ea2328f38709de26007906c5ed0d9
firstnuel <99360864+firstnuel@users.noreply.github.com>
2024-11-07 10:45:25 +0200

    Added Point text func

---
+++ point_text.go
@@ -0,0 +1,17 @@
+package sprint
+
+import "fmt"
+
+type Point struct {
+	X    float32
+	Y    float32
+	Text string
+}
+
+func PointText(p Point) Point {
+	return Point{
+		X:    p.X,
+		Y:    p.Y,
+		Text: fmt.Sprintf("Text at (%v, %v)", p.X, p.Y),
+	}
+}
