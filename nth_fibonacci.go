68f6f4f70f86ca0c05de15e749067e30c52bddfc
firstnuel <99360864+firstnuel@users.noreply.github.com>
2024-11-11 10:52:23 +0200

    Added nth fibonacci soln

--- nth_fibonacci.go
+++ nth_fibonacci.go
@@ -2,10 +2,13 @@ package sprint
 
 func NthFibonacci(index int) int {
 	if index < 0 {
+		return -1
+	}
+	if index == 0 {
 		return 0
 	}
 	if index == 1 {
-		return 0
+		return 1
 	}
-	return 1 + NthFibonacci(index-1)
+	return NthFibonacci(index-1) + NthFibonacci(index-2)
 }
