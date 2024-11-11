fc2c5516fbd2a507b4709aeadadf6e3406efe7c8
firstnuel <99360864+firstnuel@users.noreply.github.com>
2024-11-11 10:05:17 +0200

    added to pwr iterative

---
+++ to_the_power_iterative.go
@@ -0,0 +1,13 @@
+package sprint
+
+func ToThePowerIterative(n int, power int) int {
+	pwr := 1
+
+	if n < 0 || n > 20 {
+		return 0
+	}
+	for i := 0; i < power; i++ {
+		pwr *= n
+	}
+	return pwr
+}
