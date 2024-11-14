2f2b836dd7e3741786446a23584f81b9339af62d
firstnuel <99360864+firstnuel@users.noreply.github.com>
2024-11-14 03:50:07 +0200

    chill

--- payout.go
+++ payout.go
@@ -4,7 +4,7 @@ func Payout(amount int, denominations []int) (payout []int) {
 	if amount <= 0 {
 		return payout
 	}
-
+	denominations = SortInt(denominations)
 	n := len(denominations) - 1
 	for amount >= 0 {
 		if amount-denominations[n] >= 0 {
@@ -19,4 +19,24 @@ func Payout(amount int, denominations []int) (payout []int) {
 	}
 
 	return payout
+}
+
+func SortInt(arr []int) []int {
+	n := len(arr)
+	for {
+		swapped := false
+
+		for i := 0; i < n-1; i++ {
+
+			if arr[i] > arr[i+1] {
+				arr[i], arr[i+1] = arr[i+1], arr[i]
+				swapped = true
+			}
+		}
+		if !swapped {
+			break
+		}
+		n--
+	}
+	return arr
 }
