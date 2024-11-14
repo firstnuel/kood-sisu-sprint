ac819eafa79a5acef3dbc80be9725b145a73d974
firstnuel <99360864+firstnuel@users.noreply.github.com>
2024-11-14 03:23:43 +0200

    Added longest climb

--- longest_climb.go
+++ longest_climb.go
@@ -1,24 +1,19 @@
 package sprint
 
 func LongestClimb(arr []int) []int {
-	start, count, lngstStart, lngstCount := 0, 0, 0, 0
+	count, lngstCount := 0, 0
+	lngstStart := 0
 
-	for i := 0; i < len(arr)-2; i++ {
+	for i := 0; i < len(arr)-1; i++ {
 		if arr[i] < arr[i+1] {
 			count++
-			start = i
-			if count >= lngstCount {
-				lngstStart = start - (count - 1)
-			}
-		} else if arr[i] > arr[i+1] {
 			if count > lngstCount {
 				lngstCount = count
+				lngstStart = i - (count - 1)
 			}
+		} else {
 			count = 0
 		}
-	}
-	if lngstStart+lngstCount >= len(arr)-2 {
-		return arr[lngstStart:]
 	}
 	return arr[lngstStart : lngstStart+lngstCount+1]
 }
