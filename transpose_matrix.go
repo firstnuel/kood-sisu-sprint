b20c6dc809b02aa19057ad33098e4b05f2b28ebf
firstnuel <99360864+firstnuel@users.noreply.github.com>
2024-11-15 14:45:39 +0200

    second to last task

---
+++ transpose_matrix.go
@@ -0,0 +1,16 @@
+package sprint
+
+func TransposeMatrix(matrix [][]int) [][]int {
+	rows := len(matrix)
+	cols := len(matrix[0])
+	transposed := make([][]int, cols)
+	for i := range transposed {
+		transposed[i] = make([]int, rows)
+	}
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			transposed[j][i] = matrix[i][j]
+		}
+	}
+	return transposed
+}
